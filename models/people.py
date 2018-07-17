200cc0c414b856d0bbf237c4487bbf3f163f270e
Kyle L. Jensen <kljensen@gmail.com>
2018-07-17 16:37:24 +0800

    Fix Ilana's title

--- models/people.py
+++ models/people.py
@@ -87,7 +87,7 @@ people = [
         'name': u'Ilana Kelsey',
         'alum': False,
         'photo': u'Ilana_photo-small.jpg',
-        'position': u'Post-doc Associate',
+        'position': u'Postdoctoral Associate',
         'description': u'Ilana is interested in the polarization process of macrophages, specifically how individual cells activate and regulate their polarization responses when they encounter complex or conflicting cues from the environment. Using live-cell microscopy and flow cytometry, she is investigating the signaling events, gene transcription, and protein production of various polarization pathways to elucidate the plasticity and mechanism of polarization decisions in complex environments, such as might be encountered in the tumor microenvironment. Ilana received her Ph.D. from Harvard University, where she studied the regulation of the proto-oncogene PIM3 by mTORC1 in Brendan Manning’s lab.  She received Bachelor’s degrees in Biochemistry and Biology at the University of Maryland.',
     },
 
